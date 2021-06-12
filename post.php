ddf836f93d222b0a112216ee7814782cc3728567
UmidAbdu <abdugaffarov23@gmail.com>
2021-06-12 12:52:04 +0500

    working with categories

--- post.php
+++ post.php
@@ -27,7 +27,7 @@ include "includes/header.php";
             }
 
             //querying database
-            $query = "SELECT * FROM posts";
+            $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
             $select_all_posts_query = mysqli_query($connection, $query);
 
             while($row = mysqli_fetch_assoc($select_all_posts_query)){
